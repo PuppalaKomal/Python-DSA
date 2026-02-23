b45b673a6fa783106d7fa5f36bfa63461050ea48
PuppalaKomal <komalpuppala@gmail.com>
2026-02-23 15:32:18 +0530

    initial commit

--- print.numbersum.py
+++ print.numbersum.py
@@ -7,3 +7,15 @@ def main():
     print_numbers_sum(n)
 if __name__ == "__main__":
     main()
+
+# Sum of First n natural numbers
+def calculate_sum(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum += i
+    return sum
+def main():
+    n = int(input("Enter the value of n: "))
+    print("Sum of first",n,"natural numbers is:",calculate_sum(n))
+if __name__ == "__main__":
+    main()
