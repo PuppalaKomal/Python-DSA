fad12ab52e0f5a7cd62574c1e942b07cdc8abe32
PuppalaKomal <komalpuppala@gmail.com>
2026-02-23 15:13:50 +0530

    initial commit

---
+++ print.numbersum.py
@@ -0,0 +1,9 @@
+# Print numbers from 1 to n
+def print_numbers_sum(n):
+    for i in range(1,n+1):
+        print(i)
+def main():
+    n = int(input("Enter the value of n: "))
+    print_numbers_sum(n)
+if __name__ == "__main__":
+    main()
